9ba18750206b9f476bedd46bc3d3720e36e8ec92
Tychio <code@tychio.net>
2017-06-15 23:28:48 +0800

    group chinese faking letter

--- src/components/Painter.vue
+++ src/components/Painter.vue
@@ -22,13 +22,33 @@ export default {
   },
   watch: {
     word: function () {
-      const length = this.word.text.length;
-      this.ctx.font = 450-length*30 + 'px Avenir'
       this.ctx.clearRect(0, 0, this.width, this.height)
-      this.ctx.fillText(this.word.text, this.width/2, this.height/2)
+      const wordText = this.word.text
+      const rightSign = '|'
+      const bottomSign = '_'
+      if (wordText.indexOf(rightSign) > 0) {
+        const splited = wordText.split(rightSign)
+        const letter = splited[0]
+        const radical = splited[1]
+        this.ctx.font = '400px Avenir'
+        this.ctx.fillText(letter, this.width*.35, this.height/2)
+        this.ctx.fillText(radical, this.width*.8, this.height/2)
+      } else if (wordText.indexOf(bottomSign) > 0) {
+        const splited = wordText.split(bottomSign)
+        const letter = splited[0]
+        const radical = splited[1]
+        this.ctx.font = '400px Avenir'
+        this.ctx.fillText(letter, this.width/2, this.height*.4)
+        this.ctx.fillText(radical, this.width/2, this.height)
+      } else {
+        const length = wordText.length
+        this.ctx.font = 450-length*30 + 'px Avenir'
+        this.ctx.fillText(wordText, this.width/2, this.height/2)
+      }
+
       this.$emit('done', {
         url: this.$refs.canvas.toDataURL(),
-        text: this.word.text
+        text: wordText
       })
     }
   }
